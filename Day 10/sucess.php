fa2a798ed4966ecc1bb7cd548e6f230575236b4d
paridhiagarwal151-ctrl <paridhiagarwal151@gmail.com>
2026-07-09 13:28:11 +0530

    Initial commit

---
+++ Day 10/sucess.php
@@ -0,0 +1,14 @@
+ <?php
+include("header.php");
+?>
+<div class="container mt-5 text-center">
+    <div class="alert alert-success">
+        <h3>RegestrationSuccessfull</h3>
+        <p>your form has been submittedSuccessfullu.</p>
+</div>
+<a href="login.php" class="btn btn-primary">GO BACK</a>\
+</div>
+
+<?php
+include("footer.php");
+?>

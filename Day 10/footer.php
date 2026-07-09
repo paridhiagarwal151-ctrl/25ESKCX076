4ca4026fdb8bc6eb10b1b7d6d8cf5e9e08c8133a
paridhiagarwal151-ctrl <paridhiagarwal151@gmail.com>
2026-07-09 14:03:56 +0530

    Delete Day 10 directory

--- Day 10/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class ="bg- dark text-white text-center py-3 mt-5>
-<p class = "mb-0" >& copy; <?php echo data("Y");?>My website. All Rights Reserved.</p>
-</footer>
-<!-- Boot strap JS -->
- <script scr = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>

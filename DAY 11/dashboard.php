10c6bd503af8e18604f2a777ecbc9199b6fbdb37
paridhiagarwal151-ctrl <paridhiagarwal151@gmail.com>
2026-07-13 21:23:55 +0530

    Create dashboard.php

---
+++ DAY 11/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+include ("dashboardheader.php");
+include("dashboardverticalcontent.php");
+?>
+
+
+
+
+
+
+    <h2><?php echo "welcome, ".$_SESSION['user_name'] . "!";?>
+</h2>
+
+
+
+<?php
+include("dashboardfooter.php");
+include ("footer.php");
+?>
